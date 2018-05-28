5ab978717fe68c5c3d687d0024b1d0f7ae3a7c78
Hoshoyo <pveiga1991@hotmail.com>
2018-05-27 22:17:35 -0300

    broken union literals, still

--- src/c_backend.cpp
+++ src/c_backend.cpp
@@ -144,7 +144,11 @@ void C_Code_Generator::emit_type(Type_Instance* type, Token* name){
         case KIND_STRUCT: {
             assert_msg(type->flags & TYPE_FLAG_INTERNALIZED, "tried to emit uninternalized type\n");
             // TODO(psv): does this always work in C?
-            sprint("struct ");
+			if (type->flags & TYPE_FLAG_UNION) {
+				sprint("union ");
+			} else {
+				sprint("struct ");
+			}
             if(name){
                 sprint("%.*s", TOKEN_STR(name));
             } else {
@@ -241,7 +245,11 @@ void C_Code_Generator::emit_decl(Ast* decl, bool forward) {
             //sprint(" %.*s", TOKEN_STR(decl->decl_constant.name));
         }break;
         case AST_DECL_STRUCT:{
-            sprint("typedef struct %.*s{", TOKEN_STR(decl->decl_struct.name));
+			if (decl->decl_struct.flags & STRUCT_FLAG_IS_UNION) {
+				sprint("typedef union %.*s{", TOKEN_STR(decl->decl_struct.name));
+			} else {
+				sprint("typedef struct %.*s{", TOKEN_STR(decl->decl_struct.name));
+			}
             size_t nfields = decl->decl_struct.fields_count;
             for(size_t i = 0; i < nfields; ++i){
                 emit_decl(decl->decl_struct.fields[i]);
@@ -887,7 +895,11 @@ int C_Code_Generator::c_generate_top_level(Ast** toplevel, Type_Instance** type_
     for(size_t i = 0; i < array_get_length(type_decl_arr); ++i){
         Ast* decl = type_decl_arr[i];
         if(decl->node_type == AST_DECL_STRUCT) {
-            sprint("typedef struct %.*s %.*s;\n", TOKEN_STR(decl->decl_struct.name), TOKEN_STR(decl->decl_struct.name));
+			if (decl->decl_struct.flags & STRUCT_FLAG_IS_UNION) {
+				sprint("typedef union %.*s %.*s;\n", TOKEN_STR(decl->decl_struct.name), TOKEN_STR(decl->decl_struct.name));
+			} else {
+				sprint("typedef struct %.*s %.*s;\n", TOKEN_STR(decl->decl_struct.name), TOKEN_STR(decl->decl_struct.name));
+			}
         }
     }
 
